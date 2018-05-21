e06b9e5131bc12fcf2d430ff7bd6ce84d0d898fa
Toni Mueller <dev@tonimueller.org>
2018-05-21 15:47:30 +0100

    reorg

--- tests/test_configloader.py
+++ tests/test_configloader.py
@@ -1,5 +1,4 @@
 
 def test_load_config():
-    from scripts import load_config, configfile
-    c = load_config(open(configfile))
-    assert c is not None
+    from main.config import config
+    assert hasattr(config, 'api_url')
