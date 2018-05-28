9f609fb7a2512427b32470ddbf4512d1167fa732
Toni Mueller <dev@tonimueller.org>
2018-05-28 23:58:16 +0100

    keep form values across submissions

--- src/main/auth.py
+++ src/main/auth.py
@@ -1,4 +1,5 @@
 from flask import session
+from flask import request
 from flask import render_template
 from flask.views import View
 
@@ -37,6 +38,7 @@ class User:
 
 class LoginView(View):
     """Display the login form."""
+    methods = ("GET", "POST")
 
     def __init__(self, template_name):
         self.template_name = template_name
@@ -46,4 +48,6 @@ class LoginView(View):
 
     def dispatch_request(self):
         context = {'username': "", 'password': ''}
+        # import pdb; pdb.set_trace()
+        context.update(request.values.to_dict())
         return self.render_template(context)
