acf4e9bc1996ae72540f4e971253d3c0be99820d
Toni Mueller <dev@tonimueller.org>
2018-05-29 00:00:59 +0100

    get rid of pdb

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,7 +1,7 @@
 from .app import app
 from .auth import LoginView
 
-# import pdb; pdb.set_trace()
+
 app.add_url_rule('/login', view_func=LoginView.as_view(
     'login_page', template_name='login.html.j2'))
 
