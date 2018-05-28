23ffa41d12f738ab9e647edd76dac324fae4e549
Toni Mueller <dev@tonimueller.org>
2018-05-28 23:34:06 +0100

    authentication method + view

---
+++ src/main/auth.py
@@ -0,0 +1,49 @@
+from flask import session
+from flask import render_template
+from flask.views import View
+
+
+# from flask_login import UserMixin
+from .libtaiga import authenticate
+
+
+class User:
+    """What is a single user?
+
+       A user in this context is the JSON result returned from
+       successfully authenticating against Taiga.
+    """
+    auth_url = None
+
+    def __init__(self, username, password):
+        self.username = username
+        data = authenticate(auth_url, username, password)
+        if '_error_message' in data:
+            raise ValueError("user %s: %s" % (username, data['_error_message']))
+        else:
+            self.data = data
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    def get_id(self):
+        return self.data['uuid']
+
+    @classmethod
+    def set_url(cls, url):
+        cls.auth_url = url
+
+
+class LoginView(View):
+    """Display the login form."""
+
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def render_template(self, context):
+        return render_template(self.template_name, **context)
+
+    def dispatch_request(self):
+        context = {'username': "", 'password': ''}
+        return self.render_template(context)
