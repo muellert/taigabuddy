33488d6d53b512025e92d939ec03d084f995383b
Toni Mueller <dev@tonimueller.org>
2018-05-29 12:47:47 +0100

    add a logout view

--- src/main/auth.py
+++ src/main/auth.py
@@ -8,9 +8,6 @@ from flask import make_response
 from flask import redirect
 from flask import abort
 from flask.views import View
-
-
-# from flask_login import UserMixin
 from .libtaiga import authenticate
 
 
@@ -44,7 +41,7 @@ class User:
 
 
 class LoginView(View):
-    """Display the login form."""
+    """Display the login form and log the user in."""
     methods = ("GET", "POST")
 
     def __init__(self, template_name):
@@ -63,7 +60,6 @@ class LoginView(View):
                              context['password'])
             print("after authentication: ", r)
             if r is not None:
-                print("updating context")
                 flash("User %s logged in" % r['full_name'])
                 context['debug_message'] = str(r)
                 uuid = r['uuid']
@@ -74,4 +70,34 @@ class LoginView(View):
                 abort(401)
         if not response:
             response = make_response(self.render_template(context))
+        return response
+
+
+class LogoutView(View):
+    """Log the current user out by deleting his session entry
+       and unsetting his cookie.
+    """
+    methods = ("GET", "POST")
+
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def render_template(self, context):
+        return render_template(self.template_name, **context)
+
+    def dispatch_request(self):
+        u = None
+        context = {'username': "anonymous user"}
+        response = None
+        try:
+            # import pdb; pdb.set_trace()
+            u = request.cookies.get('username', None)
+            current_app.logger.info("user: %s" % u)
+            context = {'username': session[u]['full_name']}
+            del session[u]
+        except:
+            flash("You were not logged in, anyway")
+        response = make_response(self.render_template(context))
+        # delete the cookie:
+        response.set_cookie('username', '', 0)
         return response
