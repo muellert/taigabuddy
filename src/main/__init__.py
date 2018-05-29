5e9f776fd2d402f55439c8a17ae9d34c9cf64ca6
Toni Mueller <dev@tonimueller.org>
2018-05-29 12:47:55 +0100

    and hook it up

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,7 +1,11 @@
 from .app import app
 from .auth import LoginView
+from .auth import LogoutView
 
 app.add_url_rule('/login', view_func=LoginView.as_view(
     'login', template_name='login.html.j2'))
+
+app.add_url_rule('/logout', view_func=LogoutView.as_view(
+    'logout', template_name='logout.html.j2'))
 
 
