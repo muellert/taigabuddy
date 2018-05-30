6c78910e5665053bb83fa585f926fc40349af974
Toni Mueller <dev@tonimueller.org>
2018-05-30 19:48:27 +0100

    rewrite the test in a functional manner

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def test_admin_login():
     """Testing the login process with a test client"""
     from main import app
@@ -5,8 +7,6 @@ def test_admin_login():
         rv = c.post("/login", data=dict(username="admin",
                                         password="123123"))
         cookies = rv.headers.get_all('Set-Cookie')
-        result = False
-        for c in cookies:
-            if 'username=' in c:
-                result = True
+        result = reduce((lambda x, y: x or y),
+                        map((lambda x: "username" in x), cookies))
         assert result
