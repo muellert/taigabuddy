ed9806208aabd9534f604e125cf1af38e2397b2a
Toni Mueller <dev@tonimueller.org>
2018-06-02 12:17:16 +0100

    verify the basic mechanics of the Taiga client

--- tests/test_taigaclient.py
+++ tests/test_taigaclient.py
@@ -1,6 +1,4 @@
 
-
-
 def test_api_get_projects():
     """tests the Taiga client class for getting the list of projects
        from Taiga
@@ -12,9 +10,11 @@ def test_api_get_projects():
     password = '123123'
     api = app.config['API_URL']
     with app.test_request_context():
-        tg = TaigaGlobal(app, username, password)
+        tg = TaigaGlobal(app)
+        tg.login(username, password)
         assert tg is not None
         res = tg.get(api + "/projects")
         assert res is not None
         print("len(result): ", len(res.json()))
         print("result: ", res.json())
+        print("current user: ", tg.user.name)
