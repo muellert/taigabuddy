3f64ea45689a68890dd3626163d33d34d69a76fe
Toni Mueller <dev@tonimueller.org>
2018-06-04 11:59:42 +0100

    utility functions, so far probably unused

--- src/main/libutils.py
+++ src/main/libutils.py
@@ -1,18 +1,35 @@
 from flask import session
+from urllib.parse import urlparse, urljoin
+from flask import request, url_for
 
 
 def get_user_uuid(r, attr="username"):
-    return r.cookies.get(attr, None)
-
+    # import pdb; pdb.set_trace()
+    cookie = r.cookies.get(attr, None)
+    # assert cookie is not None
+    print("cookie: ", cookie)
+    print("all cookies: ", r.cookies)
+    print("all headers: ", r.headers)
+    print("session: ", session)
+    return cookie
 
 def get_user_authtoken(r, username):
     uuid = get_user_uuid(r)
     u = session[uuid]
-    return u['auth_token']
+    return u.data['auth_token']
 
 
 def set_username_cookie(resp, uuid, exp=-1):
+    if not uuid:
+        return
     if exp < 0:
         resp.set_cookie('username', uuid)
     else:
         resp.set_cookie('username', uuid, exp)
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
