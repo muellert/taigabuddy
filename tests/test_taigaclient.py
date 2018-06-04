23bb97a2df799b85f73881050285fd728dec84ee
Toni Mueller <dev@tonimueller.org>
2018-06-04 21:43:14 +0100

    refactor the login process

--- tests/test_taigaclient.py
+++ tests/test_taigaclient.py
@@ -1,9 +1,5 @@
 
-def test_api_get_projects():
-    """tests the Taiga client class for getting the list of projects
-       from Taiga
-    """
-    import requests as req
+def taiga_login():
     from main import app
     from main import tc
     username = 'admin'
@@ -11,6 +7,18 @@ def test_api_get_projects():
     api = app.config['API_URL']
     with app.test_request_context():
         tc.login(username, password)
+    return api
+
+
+def test_api_get_projects():
+    """tests the Taiga client class for getting the list of projects
+       from Taiga
+    """
+    import requests as req
+    from main import app
+    from main import tc
+    api = taiga_login()
+    with app.test_request_context():
         res = tc.get(api + "/projects")
         assert res is not None
         print("len(result): ", len(res.json()))
